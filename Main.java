c8c793c72fb1e28239fa7cde1714e111b09c65d7
oggtt <coke1.5and.coffee@gmail.com>
2026-05-21 19:10:06 +0900

    First Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("ohayo");
+
+    }
+
+    static void printf() {
+        try {
+
+            System.out.println("konabanwa");
+
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+}
